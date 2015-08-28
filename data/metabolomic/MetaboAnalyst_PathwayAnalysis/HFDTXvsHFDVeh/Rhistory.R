97d605241cbf90cda26be23cfa9d5228bfead420
Quynh Tran <qtran1@uthsc.edu>
2015-08-28 09:50:54 -0500

    Added Pathway analysis for metabolomic data

---
+++ data/metabolomic/MetaboAnalyst_PathwayAnalysis/HFDTXvsHFDVeh/Rhistory.R
@@ -0,0 +1,8 @@
+# Below is your R command history : 
+InitDataObjects("conc", "pathora", FALSE)
+Setup.MapData(tmp.vec);
+CrossReferencing("name");
+SetMetabolomeFilter(F);
+GetCircleInfo()
+RerenderMetPAGraph("zoom1440676193924.png",480.0, 420.0)
+SaveTransformedData()
